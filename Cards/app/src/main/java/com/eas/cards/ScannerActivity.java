a7e198e4e38edbeeab33c94d3286ea261adba646
esemkej <kubofulajtar@gmail.com>
2026-01-26 12:55:40 +0100

    Source code

---
+++ Cards/app/src/main/java/com/eas/cards/ScannerActivity.java
@@ -0,0 +1,153 @@
+package com.eas.cards;
+
+import android.animation.*;
+import android.app.*;
+import android.app.Activity;
+import android.content.*;
+import android.content.SharedPreferences;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.LinearLayout;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import com.budiyev.android.codescanner.*;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.getkeepsafe.taptargetview.*;
+import com.google.zxing.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+import org.json.*;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ScannerActivity extends AppCompatActivity {
+	
+	private CodeScanner mCodeScanner;
+	
+	private LinearLayout parent;
+	private CodeScannerView scanner_view;
+	
+	private SharedPreferences shared;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.scanner);
+        final View root = findViewById(R.id.parent);
+
+		final int pL = root.getPaddingLeft();
+		final int pT = root.getPaddingTop();
+		final int pR = root.getPaddingRight();
+		final int pB = root.getPaddingBottom();
+
+		ViewCompat.setOnApplyWindowInsetsListener(root, (v, insets) -> {
+			Insets bars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+			v.setPadding(
+				pL + bars.left,
+				pT + bars.top,
+				pR + bars.right,
+				pB + bars.bottom
+			);
+			return insets;
+		});
+
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		parent = findViewById(R.id.parent);
+		scanner_view = findViewById(R.id.scanner_view);
+		shared = getSharedPreferences("saveData", Activity.MODE_PRIVATE);
+	}
+	
+	private void initializeLogic() {
+		CodeScannerView scannerView = findViewById(R.id.scanner_view);
+		mCodeScanner = new CodeScanner(this, scannerView);
+		mCodeScanner.setDecodeCallback(new DecodeCallback() {
+			@Override public void onDecoded(@NonNull final Result result) { runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						shared.edit().putString("code", result.getText()).commit();
+						shared.edit().putString("type", result.getBarcodeFormat().toString()).commit();
+						finish();
+					} }
+				
+				); }
+			
+		}
+		
+		);
+		mCodeScanner.startPreview();
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
